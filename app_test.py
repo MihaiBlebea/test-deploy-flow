ea4994d4bd8254929d0afb5d6658116b9504197e
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-03-16 12:24:17 +0000

    asdasd

--- app_test.py
+++ app_test.py
@@ -5,7 +5,7 @@ class TestApp(unittest.TestCase):
 
     def test_name_is(self):
         self.people = People("Mihai", "Blebea")
-        self.assertEqual(self.people.name_is(), "Mihaia Blebea")
+        self.assertEqual(self.people.name_is(), "Mihai Blebea")
 
 if __name__ == '__main__':
     unittest.main()
